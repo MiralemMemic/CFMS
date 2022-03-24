f57580a440db6876f8eeedf80e5a0463cd136d31
Miralem Memić <56544861+MiralemMemic@users.noreply.github.com>
2022-03-25 00:54:56 +0100

    added swagger to message service

--- Microservices/message-service/src/main/java/com/example/message/MessageServiceApplication.java
+++ Microservices/message-service/src/main/java/com/example/message/MessageServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.message;
 
 import com.example.message.model.Message;
 import com.example.message.repository.MessageRepository;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class MessageServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
