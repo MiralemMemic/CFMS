1baba4b9d5549058e2631da740a0e820f02c6e3e
Miralem Memić <56544861+MiralemMemic@users.noreply.github.com>
2022-03-25 00:51:34 +0100

    added swagger to notification service

--- Microservices/notification-service/src/main/java/com/project/notification/NotificationServiceApplication.java
+++ Microservices/notification-service/src/main/java/com/project/notification/NotificationServiceApplication.java
@@ -2,12 +2,14 @@ package com.project.notification;
 
 import com.project.notification.model.Notification;
 import com.project.notification.repository.NotificationRepository;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class NotificationServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
