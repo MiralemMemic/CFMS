e154859c9b4e10141ee49ffa0a63ae57a065755d
KHalilovic3 <khalilovic3@etf.unsa.ba>
2022-05-06 02:09:26 +0200

    Zavrseno loggiranje na svaki servis

--- Microservices/notification-service/src/main/java/com/project/notification/interceptor/NotificationInterceptorConfig.java
+++ Microservices/notification-service/src/main/java/com/project/notification/interceptor/NotificationInterceptorConfig.java
@@ -1,18 +1,20 @@
 package com.project.notification.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Component
-public class NotificationInterceptorConfig extends WebMvcConfigurationSupport {
+@Configuration
+public class NotificationInterceptorConfig implements WebMvcConfigurer {
 
     @Autowired
     NotificationInterceptor notificationInterceptor;
 
     @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(notificationInterceptor);
     }
 }
