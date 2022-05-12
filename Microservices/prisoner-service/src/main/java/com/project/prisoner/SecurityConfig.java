b056885f32a9d703c29d805a51e5b8170234ed39
Faris <44686174+fmehmedovi1@users.noreply.github.com>
2022-05-12 18:19:20 +0200

    Added security config
    Role Based Access Control in Spring Boot with Keycloak

---
+++ Microservices/prisoner-service/src/main/java/com/project/prisoner/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.project.prisoner;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakAuthenticationException;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@Configuration
+@EnableWebSecurity
+@KeycloakConfiguration
+@Import(KeycloakSpringBootConfigResolver.class)
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(buildSessionRegistry());
+    }
+
+    @Bean
+    protected SessionRegistry buildSessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+
+
+        super.configure(http);
+        http
+                .authorizeRequests()
+            //    .antMatchers("/api/v1/prisoners*").hasRole("user")
+                .anyRequest().permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+    }
+
+}
--- Microservices/prisoner-service/src/main/java/com/project/prisoner/controller/PrisonerController.java
+++ Microservices/prisoner-service/src/main/java/com/project/prisoner/controller/PrisonerController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
@@ -24,24 +25,28 @@ public class PrisonerController {
     private PrisonerService prisonerService;
 
     @GetMapping
+    @RolesAllowed("user")
     public List<Prisoner> getAllPrisoners(){
         log.info("PrisonerController - getAllPrisoners");
         return prisonerService.getAllPrisoners();
     }
 
     @PostMapping
+    @RolesAllowed("user")
     public ResponseEntity<String> createPrisoner(@RequestBody @Valid Prisoner prisoner){
         log.info("PrisonerController - createPrisoner");
         return prisonerService.createPrisoner(prisoner);
     }
 
     @GetMapping("{id}")
+    @RolesAllowed("user")
     public ResponseEntity<Prisoner> getPrisonerById(@PathVariable long id){
         log.info("PrisonerController - getPrisonerById");
         return prisonerService.getPrisonerById(id);
     }
 
     @GetMapping("cell/{id}")
+    @RolesAllowed("user")
     public List<Prisoner> getPrisonersFromCell(@PathVariable long id){
         log.info("PrisonerController - getPrisonersFromCell");
         return prisonerService.sittingInCell(id);
@@ -49,36 +54,42 @@ public class PrisonerController {
 
 
     @GetMapping("worst")
+    @RolesAllowed("user")
     public List<Prisoner> getWorstPrisoners(){
         log.info("PrisonerController - getWorstPrisoners");
         return prisonerService.theWorst();
     }
 
     @GetMapping("most-common-offense")
+    @RolesAllowed("user")
     public Object mostCommonOffense(){
         log.info("PrisonerController - mostCommonOffense");
         return prisonerService.mostCommonOffense();
     }
 
     @GetMapping("cells")
+    @RolesAllowed("user")
     public List cellsNotEmpty(){
         log.info("PrisonerController - cellsNotEmpty");
         return prisonerService.notEmptyCells();
     }
 
     @GetMapping("coming-out")
+    @RolesAllowed("user")
     public Object commingOutFirst(){
         log.info("PrisonerController - commingOutFirst");
         return prisonerService.commingOutFirst();
     }
 
     @GetMapping("where-is-evil")
+    @RolesAllowed("user")
     public Object whereIsTheWorst(){
         log.info("PrisonerController - whereIsTheWorst");
         return prisonerService.whereIsTheWorst();
     }
 
     @DeleteMapping("{id}")
+    @RolesAllowed("user")
     public ResponseEntity<HttpStatus> deletePrisoner(@PathVariable long id){
         log.info("PrisonerController - deletePrisoner");
         return prisonerService.deletePrisoner(id);
