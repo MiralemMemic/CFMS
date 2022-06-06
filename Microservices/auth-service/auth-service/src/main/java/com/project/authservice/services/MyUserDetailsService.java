38fd5d395a74041d3202ac1af26674a8046e5590
Miralem Memić <56544861+MiralemMemic@users.noreply.github.com>
2022-06-06 12:36:49 +0200

    connected auth service with users

--- Microservices/auth-service/auth-service/src/main/java/com/project/authservice/services/MyUserDetailsService.java
+++ Microservices/auth-service/auth-service/src/main/java/com/project/authservice/services/MyUserDetailsService.java
@@ -1,17 +1,27 @@
 package com.project.authservice.services;
 
+import com.project.authservice.models.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User("mmemic2", "miralem123", new ArrayList<>());
+        ResponseEntity<UserResponse> responseEntity = restTemplate.getForEntity("http://localhost:8082/api/v1/users/name/" + username, UserResponse.class);
+        UserResponse userResponse = responseEntity.getBody();
+        return new User(userResponse.getUsername(), userResponse.getPassword(), new ArrayList<>());
     }
 }
